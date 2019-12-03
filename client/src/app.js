5a5234f5dd26cfaec4c1e5b26890323061139493
Matthew Lucas <mlucas24@icloud.com>
2019-12-03 17:47:08 -0500

    completed skeleton of top bar

--- client/src/app.js
+++ client/src/app.js
@@ -9,13 +9,31 @@ import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+import LanguageIcon from "@material-ui/icons/Language";
+import RoomOutlinedIcon from "@material-ui/icons/RoomOutlined";
 
 const useStyles = makeStyles(theme => ({
   root: {
     "& > *": {
-      margin: theme.spacing(1),
-      width: 1100
+      margin: theme.spacing(0),
+      width: 1100,
+      height: 55,
+      color: "grey",
+      background: "white"
     }
+  },
+  search: {
+    margin: theme.spacing(0),
+    width: 75,
+    height: 55,
+    background: "orange"
+  },
+  delivery: {
+    width: 240
+  },
+  underSearch: {
+    color: "grey"
   }
 }));
 
@@ -32,47 +50,119 @@ const App = props => {
   };
   return (
     <div>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            //className={mergeClasses.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" /*className={classes.title}*/>
-            Gammazon
-          </Typography>
-          <Button
-            aria-controls="simple-menu"
-            aria-haspopup="true"
-            onClick={handleClick}
-          >
-            Categories
-          </Button>
-          <Menu
-            id="simple-menu"
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-          >
-            <MenuItem onClick={handleClose}>Knives</MenuItem>
-            <MenuItem onClick={handleClose}>Transportation</MenuItem>
-            <MenuItem onClick={handleClose}>Food</MenuItem>
-          </Menu>
-          <form className={classes.root} noValidate autoComplete="off">
-            <TextField
-              id="outlined"
-              label="search"
-              variant="outlined"
-              color="secondary"
-            />
-          </form>
-        </Toolbar>
-      </AppBar>
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              //className={mergeClasses.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" /*className={classes.title}*/>
+              Gammazon
+            </Typography>
+            <Button
+              aria-controls="simple-menu"
+              aria-haspopup="true"
+              onClick={handleClick}
+            >
+              Categories
+            </Button>
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Knives</MenuItem>
+              <MenuItem onClick={handleClose}>Transportation</MenuItem>
+              <MenuItem onClick={handleClose}>Food</MenuItem>
+            </Menu>
+            <form className={classes.root} noValidate autoComplete="off">
+              <TextField
+                id="outlined"
+                label="search"
+                variant="outlined"
+                color="secondary"
+              />
+              <Button className={classes.search} variant="contained">
+                <SearchIcon />
+              </Button>
+            </form>
+            <div>
+              <Button
+                aria-controls="language-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+              >
+                <span>
+                  En <br />
+                  <LanguageIcon />{" "}
+                </span>
+              </Button>
+            </div>
+            <div>
+              <Button
+                aria-controls="Account-Lists-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+              >
+                <span>
+                  Hello, Chosen One <br />
+                  Account & Lists{" "}
+                </span>
+              </Button>
+            </div>
+            <div>
+              <Button
+                aria-controls="Orders-button"
+                aria-haspopup="false"
+                onClick={handleClick}
+              >
+                Orders
+              </Button>
+            </div>
+            <div>
+              <Button
+                aria-controls="Prime-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+              >
+                Prime
+              </Button>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <div className={classes.delivery}>
+              Deliver to Chosen one <br /> <RoomOutlinedIcon /> DUUUUUUVAL
+            </div>
+            <div className={classes.underSearch}>
+              <Typography variant="subtitle1">
+                <Button>12 days of deals</Button>
+                <Button>Chosen One's Gammazon.com</Button>
+                <Button>Browsing History</Button>
+                <Button>Prime Video</Button>
+                <Button>Help</Button>
+                <Button>Best Sellers</Button>
+                <Button>Find a Gift</Button>
+                <Button>Buy Again</Button>
+                <Button>Gift Cards</Button>
+                <Button>New Releases</Button>
+                <Button>#FoundItOnGammazon</Button>
+                <Button>Whole Foods</Button>
+              </Typography>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
     </div>
   );
 };
